7546fef3846ec5d5f5348d2d324693f31462c17a
Andrew <andrew.samy.hanna1998@gmail.com>
2025-10-28 04:22:27 +0300

    feat: Update .gitignore to exclude backend-dotnet obj/Debug files and enable Swagger UI in all environments

--- backend-dotnet/Program.cs
+++ backend-dotnet/Program.cs
@@ -26,11 +26,8 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAngular");
